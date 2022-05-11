c3195163f610b6262e59a9131065cfc6da71bda8
Rafael Mendes <rafaelmendes93@outlook.com>
2022-05-11 20:56:50 -0300

    Criação do projeto controle de estoque

---
+++ src/test/java/com/example/controleestoque/ControleEstoqueApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.controleestoque;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
