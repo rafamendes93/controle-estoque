5d73d80642fd53d8ad11d04272106ccc9a0fe814
Rafael Mendes <rafaelmendes93@outlook.com>
2022-05-12 00:21:27 -0300

    Alteração nome do pacote principal

--- src/main/java/br/com/rafael/controleestoque/ControleEstoqueApplication.java
+++ src/main/java/br/com/rafael/controleestoque/ControleEstoqueApplication.java
@@ -1,4 +1,4 @@
-package com.example.controleestoque;
+package br.com.rafael.controleestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
