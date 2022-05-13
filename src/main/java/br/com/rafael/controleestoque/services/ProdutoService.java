648c968aad301f5a6341d1f976095f5eb0b55b05
Rafael Mendes <rafaelmendes93@outlook.com>
2022-05-13 18:35:05 -0300

    Refactor de services para melhoria de mensagens de erro

--- src/main/java/br/com/rafael/controleestoque/services/ProdutoService.java
+++ src/main/java/br/com/rafael/controleestoque/services/ProdutoService.java
@@ -2,7 +2,9 @@ package br.com.rafael.controleestoque.services;
 
 import br.com.rafael.controleestoque.entities.Produto;
 import br.com.rafael.controleestoque.entities.dtos.ProdutoDto;
+import br.com.rafael.controleestoque.exceptions.NotFoundException;
 import br.com.rafael.controleestoque.mappers.ProdutoMapper;
+import br.com.rafael.controleestoque.messages.MessagesHandler;
 import br.com.rafael.controleestoque.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,20 +14,23 @@ public class ProdutoService {
 
     private final ProdutoRepository repo;
     private final ProdutoMapper mapper;
+    private final MessagesHandler messagesHandler;
 
     @Autowired
     public ProdutoService(ProdutoRepository repo,
-                          ProdutoMapper mapper) {
+                          ProdutoMapper mapper,
+                          MessagesHandler messagesHandler) {
         this.repo = repo;
         this.mapper = mapper;
+        this.messagesHandler = messagesHandler;
     }
 
-    private Produto findById(Integer id) throws Exception {
+    private Produto findById(Integer id) {
         return repo.findById(id)
-                .orElseThrow(() -> new Exception("produto.notfound"));
+                .orElseThrow(() -> new NotFoundException(messagesHandler.getMessage("produto.notfound", id)));
     }
 
-    public ProdutoDto find(Integer id) throws Exception {
+    public ProdutoDto find(Integer id) {
         return mapper.toDto(findById(id));
     }
 
@@ -33,13 +38,13 @@ public class ProdutoService {
         repo.deleteById(id);
     }
 
-    public ProdutoDto insert(ProdutoDto produtoDto) throws Exception {
+    public ProdutoDto insert(ProdutoDto produtoDto) {
         produtoDto.setId(null);
         Produto newProduto = repo.save(mapper.toEntity(produtoDto));
         return mapper.toDto(newProduto);
     }
 
-    public ProdutoDto update(Integer id, ProdutoDto produtoDto) throws Exception {
+    public ProdutoDto update(Integer id, ProdutoDto produtoDto) {
         Produto produto = findById(id);
         produtoDto.setId(produto.getId());
         return insert(produtoDto);
--- src/main/java/br/com/rafael/controleestoque/services/TipoProdutoService.java
+++ src/main/java/br/com/rafael/controleestoque/services/TipoProdutoService.java
@@ -1,6 +1,8 @@
 package br.com.rafael.controleestoque.services;
 
 import br.com.rafael.controleestoque.entities.TipoProduto;
+import br.com.rafael.controleestoque.exceptions.NotFoundException;
+import br.com.rafael.controleestoque.messages.MessagesHandler;
 import br.com.rafael.controleestoque.repositories.TipoProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,14 +11,17 @@ import org.springframework.stereotype.Service;
 public class TipoProdutoService {
 
     private final TipoProdutoRepository repo;
+    private final MessagesHandler messagesHandler;
 
     @Autowired
-    public TipoProdutoService(TipoProdutoRepository repo) {
+    public TipoProdutoService(TipoProdutoRepository repo,
+                              MessagesHandler messagesHandler) {
         this.repo = repo;
+        this.messagesHandler = messagesHandler;
     }
 
-    public TipoProduto findById(Integer id) throws Exception {
+    public TipoProduto findById(Integer id) {
         return repo.findById(id)
-                .orElseThrow(() -> new Exception("produto.notfound"));
+                .orElseThrow(() -> new NotFoundException(messagesHandler.getMessage("tipo.notfound", id)));
     }
 }
